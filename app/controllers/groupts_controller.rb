cf32ebc2840ae207954896e9a92d7348991bad0a
TamaraDimov <tamemojsoska@gmail.com>
2023-09-12 15:39:13 +0200

    controller and views added

---
+++ app/controllers/groupts_controller.rb
@@ -0,0 +1,38 @@
+class GroupsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @user = current_user
+    @groups = current_user.groups.all.includes(:payments)
+  end
+
+  def new
+    @user = current_user
+    @group = Group.new
+  end
+
+  def show
+    @user = current_user
+    @group = Group.find(params[:id])
+    @payments = @group.payments.order('created_at DESC')
+  end
+
+  def create
+    @user = current_user
+    group = Group.new(
+      name: new_group_params['name'],
+      icon: new_group_params['icon'],
+      user_id: current_user.id
+    )
+    if group.save
+      flash[:success] = 'You successfully created new Category'
+      redirect_to user_groups_path(user_id: current_user.id)
+    else
+      flash[:alert] = 'Category was not created'
+    end
+  end
+
+  def new_group_params
+    params.require(:new_group).permit(:name, :icon)
+  end
+end
---
+++ app/controllers/users/payments_controller.rb
@@ -0,0 +1,34 @@
+class PaymentsController < ApplicationController
+  # before_action :authenticate_user!
+
+  def index
+    @group = Group.find(params[:group_id])
+    @payments = @group.payments.order(created_at: :desc)
+  end
+
+  def new
+    @payment = current_user.payments.new
+    @group = Group.find(params[:group_id])
+    @groups = current_user.groups
+  end
+
+  def create
+    @payment = Payment.new(name: payment_params[:name], amount: payment_params[:amount], author_id: current_user.id)
+    @group = Group.find(params[:group_id])
+
+    if @payment.save
+      @payment_item = PaymentItem.new(payment_id: @payment.id, group_id: params[:payment_item][:group_id])
+
+      if @payment_item.save
+        redirect_to user_group_path(current_user, @group), notice: 'Payment successfully created.'
+      end
+
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def payment_params
+    params.require(:payment_item).permit!
+  end
+end
