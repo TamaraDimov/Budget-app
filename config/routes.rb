f02ce13481aea071f59a82253a4ee8377c4f03e1
TamaraDimov <tamemojsoska@gmail.com>
2023-09-13 12:44:15 +0200

    add controllers and models

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root to: "home#index"
+  devise_for :users
+  resources :users, only: [:index] do
+    resources :groups, only: %i[index new create show] do
+      resources :payments, only: %i[new create]
+    end
+  end
+  root 'users#index'
 end
