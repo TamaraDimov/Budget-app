0adafff3f2fc80bdd79c9a015b6657b53bc8a575
TamaraDimov <tamemojsoska@gmail.com>
2023-09-14 11:02:57 +0200

    add style

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,4 @@ class User < ApplicationRecord
   has_many :payments, foreign_key: :author_id, dependent: :destroy
   has_many :groups
 
-  validates :name, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
+# config/routes.rb
 Rails.application.routes.draw do
   devise_for :users
+
   resources :users, only: [:index] do
-    resources :groups, only: %i[index new create show] do
-      resources :payments, only: %i[new create]
+    resources :groups, only: [:index, :new, :create, :show] do
+      resources :payments, only: [:new, :create]
     end
   end
-  root 'users#index'
+
+  root 'users#index' # This sets the root path to the UsersController#index action.
 end
